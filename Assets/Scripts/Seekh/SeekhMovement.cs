6747fcb76bc1df03e29de49e0dbbb910dde51514
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2021-09-14 14:09:28 +0500

    rotation somewhat working, need to fix

--- Assets/Scripts/Seekh/SeekhMovement.cs
+++ Assets/Scripts/Seekh/SeekhMovement.cs
@@ -1,30 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using DG.Tweening;
 
 public class SeekhMovement : MonoBehaviour
 {
-    public float speed = 5;
+    [SerializeField] float speed = 5;
     Touch touch;
     float widthOfLevel;
     float xRightBound;
     float touchPosX;
     float newPosX;
-    //public float degree;
-    //public float animDuration;
 
     // Start is called before the first frame update
     void Start()
     {
         //FOR TESTING PURPOSE USE 1440 OTHERWISE USE SCREEN.WIDTH
-        xRightBound = 1440 / 2;
-        //xRightBound = Screen.width / 2;
+        //xRightBound = 1440 / 2;
+        xRightBound = Screen.width / 2;
 
         //WIDTH OF LEVEL IS 4.8
         widthOfLevel = 4.8f;
-
-        //transform.DORotate(new Vector3(0, 90, 0), 2f);
     }
 
     // Update is called once per frame
@@ -42,12 +37,6 @@ public class SeekhMovement : MonoBehaviour
                 newPosX = (touchPosX * widthOfLevel) / xRightBound;
 
                 transform.position = new Vector3(newPosX, transform.position.y, transform.position.z);
-                //new code starts here
-
-                //if(transform.position.x > xRightBound)
-                //{
-                //    LeanTween.rotateY(gameObject, degree, animDuration);
-                //}
             }
         }
     }
---
+++ Assets/Scripts/Seekh/SeekhRotation.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SeekhRotation : MonoBehaviour
+{
+    Touch touch;
+    float maxRightAngle = 30;
+    float touchMargin = 10;
+
+    //THIS IS THE HIGHEST DELTA I COULD GET BY SWIMPING THE SCREEN REALLY FAST. THIS VALUE CORRESPONDS TO THE touch.deltaPosition
+    float maxDelta = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.DORotate(new Vector3(0, 90, 0), 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                //Debug.Log(touch.deltaPosition);
+
+                //IF DELTA POSITION IS MAX DELTA, ROTATE IT TO MAX ANGLE, THE RATIO OF ROTATION IS DETERMINED BY THE DELTA POSITION
+                if(touch.deltaPosition.x < -touchMargin)
+                {
+                    float angle = (-maxRightAngle * touch.deltaPosition.x) / -maxDelta;
+                    Debug.Log(angle);
+                    rotateLeft(angle * 10);
+                }
+                else if(touch.deltaPosition.x > touchMargin)
+                {
+                    float angle = (maxRightAngle * touch.deltaPosition.x) / maxDelta;
+                    Debug.Log(angle);
+                    rotateRight(angle * 10);
+                }
+                else
+                {
+                    rotateReset();
+                }
+
+            }
+        }
+    }
+
+    void rotateLeft(float angle)
+    {
+        transform.DORotate(new Vector3(0, angle, 0), 2f);
+    }
+
+    void rotateRight(float angle)
+    {
+        transform.DORotate(new Vector3(0, angle, 0), 2f);
+    }
+
+    void rotateReset()
+    {
+        //Debug.Log("RESET");
+        transform.DORotate(new Vector3(0, 0, 0), 2f);
+    }
+}
