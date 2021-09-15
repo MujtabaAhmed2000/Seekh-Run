1b8156bf745cc6392625233ca640494e68b3d6d8
Meer-Balaj <mbalaj21@gmail.com>
2021-09-15 21:37:05 +0500

    Added obstacles
    plus path texture

--- Assets/Scripts/Wall/WallBang.cs
+++ Assets/Scripts/Wall/WallBang.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class WallBang : MonoBehaviour
 {
-    //[HideInInspector] public ParticleSystem shatter;
+    [HideInInspector] public ParticleSystem shatter;
+
     private void Start()
     {
-        //shatter = GetComponent<ParticleSystem>();
+
+        shatter = GetComponent<ParticleSystem>();
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            //transform.GetChild(0)
-            //shatter.Play();
+            shatter.Play();
         }
     }
 }
