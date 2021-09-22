1acfeedfbd088d276cc339f14d67b40ca4471d7a
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2021-09-22 09:35:41 +0500

    stove turns on and off

--- Assets/Scripts/Seekh/SeekhCollision.cs
+++ Assets/Scripts/Seekh/SeekhCollision.cs
@@ -32,4 +32,12 @@ public class SeekhCollision : MonoBehaviour
             zPositionOnSeekh += gapBetweenItems;
         }
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Stove")
+        {
+            //Debug.Log("INSIDE STOVE");
+        }
+    }
 }
