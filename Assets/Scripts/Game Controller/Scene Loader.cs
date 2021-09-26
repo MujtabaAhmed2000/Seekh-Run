83998af49efc6a818f52d6796e4705d350103616
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2021-09-26 13:51:46 +0500

    scene loader

---
+++ Assets/Scripts/Game Controller/Scene Loader.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    public static void loadLevel1()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public static void loadLevel2()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+    public static void loadLevel3()
+    {
+        SceneManager.LoadScene("Level 3");
+    }
+
+    public static void loadLevel4()
+    {
+        SceneManager.LoadScene("Level 4");
+    }
+
+    public static void loadLevel5()
+    {
+        SceneManager.LoadScene("Level 5");
+    }
+}
