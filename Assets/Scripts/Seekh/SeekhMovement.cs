0abcbfbb0cff0e5942c24e71043ccdb7b125e464
MujtabaAhmed2000 <61231702+MujtabaAhmed2000@users.noreply.github.com>
2021-09-29 23:23:37 +0500

    Update SeekhMovement.cs

--- Assets/Scripts/Seekh/SeekhMovement.cs
+++ Assets/Scripts/Seekh/SeekhMovement.cs
@@ -91,7 +91,7 @@ public class SeekhMovement : MonoBehaviour
     {
         for(int i = 0; i < 360; i++)
         {
-            skewer.Rotate(new Vector3(0, 0, -1));
+            skewer.Rotate(new Vector3(0, 0, -2));
 
             yield return new WaitForSeconds(0.01f);
         }
